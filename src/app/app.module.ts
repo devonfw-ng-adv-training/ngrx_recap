e912080608ec77b3759df32a4085e99130d34721
Rohan Fernandes <rohan.b.fernandes@capgemini.com>
2025-08-03 20:13:20 +0200

    add counter store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,18 @@ import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { CounterLocalComponent } from './counter-local/counter-local.component';
+import { counterReducer } from './store/counter.reducer';
+import { CounterStoreComponent } from './counter-store/counter-store.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CounterLocalComponent
+    CounterLocalComponent,
+    CounterStoreComponent
   ],
   imports: [
     BrowserModule,
-    StoreModule.forRoot({}, {}),
+    StoreModule.forRoot({ counter: counterReducer}),
     StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() })
   ],
   providers: [],
